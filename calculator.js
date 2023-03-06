0a39a8f7d4de878beafff2f55e15073be65be15b
Estefania Ramos S <124093052+EstefaniaR5@users.noreply.github.com>
2023-03-06 15:34:44 -0600

    commit challenge

--- calculator.js
+++ calculator.js
@@ -1,18 +1,9 @@
 const calculator = {
-    sum(a, b) {
-        return a + b;
-    },
-    substract(a, b) {
-        return a - b;
-    },
-    multiply(a, b) {
-        return a * b;
-    },
-    divide(a, b) {
-        return a / b;
-    },
-    power: (a,b) => a / b,
+    sum: (a, b) => a + b,
+    substract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+    power: (a, b) => a ** b,
 }
-
 
 module.exports = calculator;
